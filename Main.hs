4f112877c03507eaf154f629c0f1fd34b1c19ce0
Jens Petersen <petersen@redhat.com>
2020-04-20 19:21:39 +0800

    more explicit imports

--- Main.hs
+++ Main.hs
@@ -1,15 +1,15 @@
 import Criterion
 import Criterion.Main
-import Data.List
+import Data.List(sort, sortBy, sortOn)
 import Data.Ord
-import GHC.Exts
+import GHC.Exts(sortWith)
 
 list :: [Int]
 list = [1..10000]
 
 main = defaultMain
   [ bench "sort"   $ nf sort (reverse list)
-  , bench "reverse . sort"   $ nf (reverse . sort) list
+  , bench "reverse . sort" $ nf (reverse . sort) list
   , bench "sortBy" $ nf (sortBy (comparing Down)) list
   , bench "sortOn" $ nf (sortOn Down) list
   , bench "sortWith" $ nf (sortWith Down) list
